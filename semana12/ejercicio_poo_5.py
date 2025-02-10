00feff67c8e97da1fbae21a56e6cdf4e782a5292
JovanBV <jovan.brenesv@gmail.com>
2025-02-10 14:55:37 -0600

    Added comments and corrected orthography

--- semana12/ejercicio_poo_5.py
+++ semana12/ejercicio_poo_5.py
@@ -1,15 +1,30 @@
+'''
+This class holds main and general information for all sub-classes, like name and max health
+'''
 class Character:
     def __init__(self, name, max_health):
-        self.name = name
-        self.max_health = max_health
-
+        try:
+            self.name = name
+            self.max_health = int(max_health)
+        except:
+            raise ValueError('Max health must be a numerical value.')
+'''
+Thic class holds only warrior traits
+'''
 class WarriorTraits:
     def __init__(self):
         self.strength = 20
-
+'''
+This class only holds mage traits
+'''
 class MageTraits:
     def __init__(self):
         self.mana = 50
+
+'''
+This class holds all that general information needed to create a Batte Mage, and also requests name
+and max health
+'''
 
 class BattleMage(Character, WarriorTraits, MageTraits):
     def __init__(self, name, max_health):
@@ -17,7 +32,7 @@ class BattleMage(Character, WarriorTraits, MageTraits):
         WarriorTraits.__init__(self)
         MageTraits.__init__(self)
 
-player_1 = BattleMage('Jovan', 30)
+player_1 = BattleMage('Jovan', '90')
 
 print(player_1.strength)
 print(player_1.max_health)
